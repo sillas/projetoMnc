7ddd42171be77bcf98536c1cbcb6146d2f93d570
Sillas S. S. Moura <deepsky.sill@gmail.com>
2015-01-30 11:20:39 -0300

    adicionar a classe Produtos - conexão com o BD
    Conecta com o BD e extrai os dados para a memória - dicionário,
    Preenche "produtos" e "categoria".

--- teste.py
+++ teste.py
@@ -1,26 +1,28 @@
-class Produtos:
-    """ * item:
+""" 
+        * item:
         - Nome do produto:
-        - dict{preço, descrição**, estoque, cod.}
+        - dict{preco, descricao**, estoque, Id_cod.}
 
         *categoria**:
         - categoria:
-        - [itens (nomes apenas)]
+        - [itens (nomes dos itens, apenas)]
 
         ** Estrutura no BD:
-        "categoria,descricao" - primeira ',' Caractere separador'
+        Formato: "categoria,descricao" 
 
 
+        CREATE TABLE produtos(
+            id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+            nome VARCHAR(150),
+            quant INTEGER,
+            preco DOUBLE
+            descricao VARCHAR(150), #500!
+       )
 
-CREATE TABLE produtos (
-id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-nome VARCHAR(150),
-quantidade INTEGER,
-descricao VARCHAR(500),
-preco DOUBLE
-)
-
+Comando correto para inserir produto:
+        c.execute( "INSERT INTO produtos (nome, quant, preco, descricao) VALUES ('%s', %d, %f,'%s')" % ("agua", 10, 2.30, "bebidas,ok"))
 """
+class Produtos:
     def __init__(self):
         self.produto = {}
         self.categoria = {}
@@ -29,26 +31,28 @@ preco DOUBLE
     def UpdateProdutoList(self):
         import MySQLdb #mudar de posicao
         try:
-            con = MySQLdb.connect(host= `host`, user=`root`, passwd=password, db=`banco`)
+            con = MySQLdb.connect(host="10.5.18.40", user="root", passwd="admin", db="loja_pet")
         except:
             print 'Erro na conexcao com o banco de dados!'
             return 'BD_Con_Erro'
         
         c = con.cursor()
-        c.execute("SELECT * FROM `produtos`")#ver
-        #preencher:
-        '''
-        for item in c: -- Ver isso
-            item, preco, descricao, estoque = item -- Ver a estrutura antes!
-            self.produto[item] = {'preco':preco, 'descricao':descricao, 'estoque':estoque}
+        c.execute("SELECT * FROM produtos")
+        
+        #preencher os dicionarios com os dados do banco para acesso rapido:
+        for item in c: 
+            id_Prod, item, preco, descricao, estoque = item
+            self.produto[item] = {'preco' : preco, 'descricao' : descricao, 'estoque' : estoque, 'id_' : id_Prod}  #preenche dict. produto
 
-            for i in range(len(descricao)):
-                if descricao[i] == ',':
-                    cat = descricao[:i]
-                    break
+            #preenche dict Categoria
+            for i in range(len(descricao)): #extrai a categoria do produto do campo produto_descricao
+                if descricao[i] == ',': #encontra o caractere separador
+                    cat = descricao[:i] #encontrou a categoria
+                    break #sai do loop
             
-            if not cat in self.categoria:
-                self.categoria[cat] = []
-            self.categoria[cat].append
-        '''
+            if not cat in self.categoria: #se a categoria ainda nao existe...
+                self.categoria[cat] = [] #cria primeiro.
+            self.categoria[cat].append #preenche a categoria.
+        
+
         
